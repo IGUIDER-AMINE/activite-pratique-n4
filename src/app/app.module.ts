5e7e892112c9c1bbdccefa5f7d78e37666f3c7b2
IGUIDER AMINE <amineiguider2017@gmail.com>
2024-04-11 21:38:13 +0200

    fini 20:11 de la partie 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppErrorsComponent } from './app-errors/app-errors.component';
 import {AppHttpInterceptor} from "./services/app-http.interceptor";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import {AppHttpInterceptor} from "./services/app-http.interceptor";
     EditProductComponent,
     DashboardComponent,
     NavbarComponent,
-    AppErrorsComponent
+    AppErrorsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
